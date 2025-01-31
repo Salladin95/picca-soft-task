4e0c6d61f57df124371d4c9eb5d55ac1d5ff35b5
Khalid <edmondantes036@gmail.com>
2025-01-31 22:24:13 +0300

    feat: add main-page-layout & update employee comp

--- src/app/RouterView.tsx
+++ src/app/RouterView.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
-import { HomePage, AddEmployee } from "~/pages"
+import { MainPageLayout } from "~/widgets"
+import { HomePage, AddEmployee, EmployeePage } from "~/pages"
 
 export function RouterView() {
 	return (
 		<BrowserRouter basename="/picca-soft">
 			<Routes>
-				<Route index path="/" element={<HomePage />} />
-				<Route path="/add-employee" element={<AddEmployee />} />
+				<Route path="/" element={<MainPageLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="/add-employee" element={<AddEmployee />} />
+					<Route path="/employee/:id" element={<EmployeePage />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ src/widgets/MainPageLayout.tsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from "react-router-dom"
+
+export function MainPageLayout() {
+	return (
+		<>
+			<header>
+				<Link to={"/"}>Employees</Link>
+			</header>
+			<Outlet />
+			<footer>FOOTER</footer>
+		</>
+	)
+}
