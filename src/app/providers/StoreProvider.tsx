7b6abcd3ffff867ed13366d946692af8a0532222
Khalid <edmondantes036@gmail.com>
2025-02-01 14:38:13 +0300

    refactor: store employees in localstore

--- src/app/providers/StoreProvider.tsx
+++ src/app/providers/StoreProvider.tsx
@@ -1,12 +1,20 @@
 import { Provider } from "react-redux"
 
 import store from "~/app/redux/store"
-import { setEmployees } from "~/app/redux"
+import { areEmployees } from "~/shared/guards"
+import { getSafeLocalStoreItem } from "~/shared/lib"
 import { EmployeeType, type WithChildren } from "~/shared/types"
+import { EMPLOYEES_LOCAL_STORE_KEYS, setEmployees } from "~/app/redux"
 
 import EMPLOYEES from "~/app/employees.json"
 
 export function StoreProvider(props: WithChildren) {
-	store.dispatch(setEmployees(EMPLOYEES as EmployeeType[]))
+	const employees = getSafeLocalStoreItem(
+		EMPLOYEES_LOCAL_STORE_KEYS.EMPLOYEES,
+		areEmployees,
+		EMPLOYEES as EmployeeType[],
+	)
+
+	store.dispatch(setEmployees(employees))
 	return <Provider store={store}>{props.children}</Provider>
 }
