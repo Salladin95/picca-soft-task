bea1da2f0081338a5301e40636a5472aee094bd3
Khalid <edmondantes036@gmail.com>
2025-02-03 14:57:45 +0300

    feat: add test for typeGuards

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
 // vitest.config.ts
+import * as path from "path";
 import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	test: {
@@ -8,5 +10,10 @@ export default defineConfig({
 		environment: "jsdom", // or 'node' if not testing DOM-related code
 		// reporters: ['html', 'verbose'], // Add reporters as needed (e.g., HTML reports)
 		// ... other Vitest options
+
+		alias: {
+			"~/": path.resolve(__dirname, "./src"),
+		},
 	},
+	plugins: [tsconfigPaths()],
 })
